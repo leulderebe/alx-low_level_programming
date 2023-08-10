12f2c700b8737a99b1b8750a9e9f75994bb133ee
leulderebe <leulderebe@gmail.com>
2023-08-10 14:02:00 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
  * main - A program that prints a line with puts function
  * return: 0 (success)
  */
-int main (void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
