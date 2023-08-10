4b6ac09210da1fae8e14b8845a8849540ed995c2
leulderebe <leulderebe@gmail.com>
2023-08-10 13:39:05 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main -A program that print a line with puts function
+ * main - A program that prints a line with puts function
  * return: 0 (success)
  */
 int main (void)
